53106fd0aa78dfd07b982ed7482d8f8315b6b8bc
IqbalFebrianwar <zonelyrics26@gmail.com>
2024-01-29 15:55:14 +0700

    Feat: Menambahkan index pada skeleton

--- src/components/home/skeleton.tsx
+++ src/components/home/skeleton.tsx
@@ -2,8 +2,8 @@ const SkeletonUI = () => {
   return (
     <div className="w-full">
       <div className="grid xl:px-20 lg:px-16 max-md:px-5 justify-center text-center mt-10 w-full gap-10 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-        {Array.from({ length: 20 }, () => (
-          <div className="shadow p-5 w-72 bg-slate-900 rounded-xl">
+        {Array.from({ length: 20 }, (_, index) => (
+          <div key={index} className="shadow p-5 w-72 bg-slate-900 rounded-xl">
             <div className="skeleton rounded-xl h-52 w-full"></div>
             <div className="flex w-full gap-x-3 items-center">
               <>
